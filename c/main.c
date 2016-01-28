6a89a5336936dc37609d44057bcb735cfac21542
Nicholas Corgan <ncorgan@ucsc.edu>
2016-01-27 17:38:46 -0800

    Removed ubigint test

--- c/main.c
+++ c/main.c
@@ -10,22 +10,5 @@ int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
 
-    ubigint_handle_t num1 = NULL;
-    ubigint_handle_t num2 = NULL;
-    ubigint_handle_t num3 = NULL;
-
-    new_ubigint_from_num(&num1, 358);
-    new_ubigint_from_string(&num2, "37");
-    new_ubigint(&num3);
-
-    print_ubigint(num1);
-    print_ubigint(num2);
-    ubigint_modulus(num1, num2, num3);
-    print_ubigint(num3);
-
-    free_ubigint(&num1);
-    free_ubigint(&num2);
-    free_ubigint(&num3);
-
     return 0;
 }
