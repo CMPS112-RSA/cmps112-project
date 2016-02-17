da5159819afdd4810b013a8b432115b9dca0c528
mike <mike@eduroam-169-233-236-10.ucsc.edu>
2016-02-17 14:52:54 -0800

    Made lots of changes ot keygen file

--- csharp/gen.cs
+++ csharp/gen.cs
@@ -4,13 +4,25 @@ using IntXLib;
 class gen {
 
   private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
-  public static IntX genRand() {
-    byte[] r = new byte[2];
+  public static ulong getBin() {
+    byte[] r = new byte[1];
     rngCsp.GetBytes(r);
-    IntX output = 1;
-    for(int i = 0; i < r.Length; i++) {
-      output = output + (i + 1) * r[i];
+
+    if(r[0] % 2 == 0) {
+      return 1;
+    }else {
+      return 0;
     }
+  }
+
+  public static IntX genRand() {
+    int size = 8;
+    IntX output = 0;
+    for(int i = 0; i < size; i++) {
+      ulong bit = (getBin() * (ulong) Math.Pow(2, i));
+      output = output + (IntX) bit;
+    }
+    Console.WriteLine(output);
     return output;
   }
 
@@ -24,17 +36,35 @@ class gen {
       }
     }
     return true;
+}
+
+public static bool isPrime_new(IntX num) {
+  if(num < 2) {
+    return false;
   }
+  if(num == 2) {
+    return false;
+  }
+  if(num % 2 == 0) {
+    return false;
+  }
+  for(ulong i = 3; i <= Math.Sqrt( (ulong) num); i += 2) {
+    if(num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
   public static IntX genRandPrime(IntX index) {
-    IntX count = 0;
-    IntX p = 0;
-    while(count < index) {
-      p = genRand();
-      if(isPrime(p)) {
-        count++;
+      IntX p = 0;
+      while(true) {
+        p = genRand();
+        Console.WriteLine(p);
+        if(isPrime(p)) {
+          break;
+        }
       }
-    }
     return p;
   }
 
@@ -48,11 +78,11 @@ class gen {
 
   public static IntX[] genN() {
     IntX[] output = new IntX[3];
-    Console.WriteLine("Generating p...");
+    //Console.WriteLine("Generating p...");
     output[0] = genRandPrime(1);
-    Console.WriteLine("Generating q...");
+    //Console.WriteLine("Generating q...");
     output[1] = genRandPrime(1);
-    Console.WriteLine("Generating n...");
+    //Console.WriteLine("Generating n...");
     output[2] = output[0] * output[1];
 
     return output;
@@ -62,7 +92,7 @@ class gen {
     IntX totient = (npq[0] - 1) * (npq[1] - 1);
 
     IntX output = 0;
-    Console.WriteLine("Generating e...");
+    //Console.WriteLine("Generating e...");
     for(IntX e = 2; e < totient; e++) {
       if(gcd(e, totient) == 1) {
         output = e;
@@ -72,9 +102,58 @@ class gen {
     return output;
   }
 
+  public static IntX[] Extended_GCD(IntX a, IntX b)
+  {
+      IntX[] result = new IntX[3];
+      if (a < b) //if a less than b, switch them
+      {
+          IntX temp = a;
+          a = b;
+          b = temp;
+      }
+      IntX r = b;
+      IntX q = 0;
+      IntX x0 = 1;
+      IntX y0 = 0;
+      IntX x1 = 0;
+      IntX y1 = 1;
+      IntX x = 0, y = 0;
+      while (r > 1)
+      {
+          r = a % b;
+          q = a / b;
+          x = x0 - q * x1;
+          y = y0 - q * y1;
+          x0 = x1;
+          y0 = y1;
+          x1 = x;
+          y1 = y;
+          a = b;
+          b = r;
+      }
+      result[0] = r;
+      result[1] = x;
+      result[2] = y;
+      return result;
+  }
+
+  public static IntX modInverse(IntX a, IntX m)
+  {
+    IntX[] g = Extended_GCD(a, m);
+    if (g.Length == 0)
+        return -1;
+    else
+    {
+        // m is added to handle negative x
+        IntX res = (g[1] % m + m) % m;
+        return res;
+    }
+}
+
+
   public static IntX genD(IntX e, IntX totient) {
     IntX output = 0;
-    Console.WriteLine("Generating d...");
+    //Console.WriteLine("Generating d...");
     for(IntX d = 3; d < totient; d++) {
       if((d*e) % totient == 1) {
         output = d;
@@ -85,11 +164,20 @@ class gen {
   }
 
   public static void Main() {
-    IntX[] npq = genN();
-    Console.WriteLine(npq[2]);
-    IntX e = genE(npq);
-    Console.WriteLine(e);
-    IntX d = genD(e, ((npq[0] -1) * (npq[1] - 1)));
-    Console.WriteLine(d);
+    IntX dp = 0;
+    while(dp < 32) {
+      IntX[] npq = genN();
+      IntX e = genE(npq);
+      //Console.WriteLine("Generating d...");
+      IntX d = modInverse(e, ((npq[0] -1) * (npq[1] - 1)));
+      dp = d;
+      Console.Write("n=");
+      Console.Write(npq[2]);
+      Console.Write(", e=");
+      Console.Write(e);
+      Console.Write(", d=");
+      Console.Write(d);
+      Console.WriteLine("");
+    }
   }
 }
--- csharp/read.cs
+++ csharp/read.cs
@@ -1,7 +1,9 @@
 using System;
 using System.IO;
 using IntXLib;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class read {
   public static readonly string[] options = {"-f", "-o", "-k"};
@@ -49,6 +51,33 @@ public class read {
     return x * y;
   }
 
+  public static bool odd(IntX a) {
+    if(a == 1) {
+      return true;
+    }else if(a == 0) {
+      return true;
+    }
+    if(a % 2 == 0) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  public static IntX fastPower(IntX a, IntX n, IntX m) {
+    IntX x = a;
+    IntX y = (odd(n)) ? a : 1;
+    IntX nprime = n / 2;
+    while (nprime > 0) {
+      x = power(x, 2) % m;
+      if(odd(nprime)) {
+        y = (y==1) ? x : (y*x) % m;
+      }
+      nprime = nprime / 2;
+    }
+    return y;
+  }
+
   public static IntX[] encrypt(byte[] message, IntX key_n, IntX key_e) {
     IntX[] output = new IntX [message.Length];
     for(uint i = 0; i < message.Length; i++) {
@@ -67,9 +96,72 @@ public class read {
     for(uint i = 0; i < message.Length; i++) {
       IntX powop = power(message[i], key_d);
       IntX t = (powop % key_n);
+      //IntX t = fastPower(message[i], key_d, key_n);
       output[i] = t;
     }
     return output;
+  }
+
+  public static void writeToFile(IntX[] encrypted, string path, IntX key_n, IntX key_d ) {
+    string[] output = new string[encrypted.Length+2];
+    output[0] = key_n.ToString();
+    output[1] = key_d.ToString();
+    int count = 2;
+    for(int i = 0; i < encrypted.Length; i++) {
+      output[count] = encrypted[i].ToString();
+      count++;
+    }
+    //System.IO.File.WriteAllText(path, key_n.ToString());
+    //System.IO.File.WriteAllText(path, key_d.ToString());
+    System.IO.File.WriteAllLines(path, output);
+  }
+
+  public static void writeDecToFile(string path, IntX[] file) {
+    byte[] output = new byte[file.Length];
+    for(int b = 0; b < output.Length; b++) {
+      output[b] = (byte) file[b];
+    }
+
+    BinaryWriter Writer = new BinaryWriter(File.OpenWrite(path));
+    Writer.Write(output);
+    Writer.Flush();
+    Writer.Close();
+
+    //File.WriteAllBytes(path, output);
+  }
+
+  public static IntX stringToInt(string str) {
+    int power = 0;
+    IntX sum = 0;
+    for(int i = str.Length - 1; i >= 0; i--) {
+      sum = sum + ((str[i] - '0') * (int) (Math.Pow(10, power)));
+      power++;
+    }
+    return sum;
+  }
+
+  public static IntX[] decryptfromFile(string path, IntX key_e) {
+    int lineCount = File.ReadLines(path).Count();
+    IntX[] fromFile = new IntX[lineCount];
+    int count = 0;
+
+    foreach (string line in File.ReadLines(path)) {
+      fromFile[count] = stringToInt(line);
+      count++;
+    }
+
+    IntX key_n = fromFile[0];
+    IntX[] output = new IntX[fromFile.Length - 2];
+    int outindex = 0;
+    for(int i = 2; i < fromFile.Length; i++) {
+      output[outindex] = fromFile[i];
+      outindex++;
+    }
+
+
+
+    IntX[] dec = decrypt(output, key_n, key_e);
+    return dec;
   }
 
   public static void Main() {
@@ -78,14 +170,15 @@ public class read {
 
       byte[] file = readFile(filePath);
       IntX[] encrypted = encrypt(file, Int64.Parse(args[2]), Int64.Parse(args[3]));
+
+      writeToFile(encrypted, args[5], Int64.Parse(args[2]), Int64.Parse(args[3]));
+
       Console.Write("--------------------------\n");
-
-      //byte[] dec = Convert.FromBase64String(enc);
-      IntX[] decrypted = decrypt(encrypted, Int64.Parse(args[2]), Int64.Parse(args[4]));
-
-      for(int i = 0; i < decrypted.Length; i++) {
-        Console.Write((char) decrypted[i]);
+      IntX[] decfile = decryptfromFile(args[5], Int64.Parse(args[4]));
+      for(int i = 0; i < decfile.Length; i++) {
+        //Console.Write((char) decfile[i]);
       }
+      writeDecToFile(args[6], decfile);
 
   }
 }
